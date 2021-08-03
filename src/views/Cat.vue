f9eccff54c319c26e67d6452f701361f789a3889
aydamacink <dayanamick@gmail.com>
2021-08-03 02:17:53 +0200

    putting the data on screen

--- src/views/Cat.vue
+++ src/views/Cat.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h1>This is a page about cats</h1>
+    <div v-if="dataReady">
+      <h2>hello</h2>
+      <h3>{{ this.items }}</h3>
+      <li v-for="item in items" :key="item.description">{{ item }}</li>
+    </div>
+
     <Hi />
   </div>
 </template>
@@ -15,11 +21,25 @@ import Hi from "@/components/Hi.vue";
   },
 })
 export default class Cat extends Vue {
-  async mounted() {
+  dataReady!: boolean;
+  items!: unknown;
+
+  data(): { dataReady: boolean; items: [] } {
+    return {
+      dataReady: false,
+      items: [],
+    };
+  }
+
+  async mounted(): Promise<unknown> {
     console.log("HEYYYYY");
     // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
     const response = await client.getEntries();
-    console.log(response.items);
+    const items = response.items;
+    this.dataReady = true;
+    console.log("data is here");
+    this.items = items;
+    return items;
   }
 }
 </script>
